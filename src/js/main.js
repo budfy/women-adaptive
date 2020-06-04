558ff676613609dad56d8a31470f4653d239a568
budfy <serdyuk.ro@gmail.com>
2020-06-04 13:33:14 +0300

    Adaptive done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
-$(function () {
+$(document).ready(function () {
+  $(".wf-header__nav-toggle").click(function (e) {
+    e.preventDefault();
 
+    $(".wf-header__nav").toggleClass("active");
+  });
+
+  $(".wf-header__nav-item").click(function (e) {
+    e.preventDefault();
+
+    $("html, body")
+      .stop()
+      .animate({
+        scrollTop: $($(this).attr("href")).offset().top - 40,
+      });
+
+    $(".wf-header__nav").removeClass("active");
+  });
 });
